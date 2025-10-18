64787ef3f41500c2b7e957747021937810aae006
thddntjr1234 <thddntjr112233@gmail.com>
2025-10-18 23:17:08 +0900

    프로젝트 초기 설정

---
+++ howkimp-server/src/main/java/com/howkimp/HowkimpServerApplication.java
@@ -0,0 +1,13 @@
+package com.howkimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowkimpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowkimpServerApplication.class, args);
+	}
+
+}
---
+++ howkimp-server/src/test/java/com/howkimp/HowkimpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howkimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowkimpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
